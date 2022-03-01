323a1736744d79fe45fd419f09003e0442bfc717
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-03-02 01:30:05 +0500

    colorschme again updated

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,9 +1,9 @@
 require("github-theme").setup({
   theme_style = "dimmed",
   function_style = "italic",
-  --[[ sidebars = {"qf", "vista_kind", "terminal", "packer"}, ]]
+  sidebars = {"qf", "vista_kind", "terminal", "packer"},
 
-  --[[ -- Change the "hint" color to the "orange" color, and make the "error" color bright red
+  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
   colors = {hint = "orange", error = "#ff0000"},
 
   -- Overwrite the highlight groups
@@ -14,5 +14,5 @@ require("github-theme").setup({
       -- this will remove the highlight groups
       TSField = {},
     }
-  end ]]
+  end
 })
