a9b15a6302678ceffa02d3568d38200b2ea85050
komildeveloper <komildeveloper111@gmail.com>
2022-03-28 15:42:58 +0500

    new options added

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -44,3 +44,10 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+vim.cmd [[
+augroup kitty_mp
+    autocmd!
+    au VimLeave * :silent !kitty @ set-spacing padding=20 margin=10
+    au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0
+augroup END
+]]
