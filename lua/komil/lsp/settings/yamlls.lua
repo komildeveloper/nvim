d3866b6fc92eaf06547d13c80737e6915e5c48e2
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2023-02-15 15:57:34 +0500

    docker-compose added

--- lua/komil/lsp/settings/yamlls.lua
+++ lua/komil/lsp/settings/yamlls.lua
@@ -6,6 +6,7 @@ return {
         ["https://json.schemastore.org/github-action.json"] = "/.github/workflows/*",
 				["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.yaml",
 				["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta8.json"] = "skaffold.yaml",
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.yml",
 			},
 		},
 	},
